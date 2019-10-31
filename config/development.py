a4340939fcfb18435b8a680a160ab945a8e875ee
Marko Knezevic <markoknezevic75@gmail.com>
2019-11-01 00:44:24 +0100

    06.3_Backend(Heroku)
    Fixed URL

--- config/development.py
+++ config/development.py
@@ -8,9 +8,7 @@ class Development(Config):
     DB_NAME = 'praksa_baza'
     DB_USER = "marko"
     DB_PASSWD = "123456"
-    DB_HOST = "127.0.0.1"
-    DB_PORT = 5432
+    # DB_HOST = "127.0.0.1"
+    # DB_PORT = 5432
 
-    SQLALCHEMY_DATABASE_URI = "postgres://qfzpkprtsifydm" \
-                              ":a206e17b674364a11d405801a1c00e237f869c5816d8658168d3b19858cf724f@ec2-54-228-252-67.eu" \
-                              "-west-1.compute.amazonaws.com:5432/dcopatjjik6tdb "
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
